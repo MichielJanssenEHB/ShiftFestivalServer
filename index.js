fdf58d9e905c22833317ba688adcc90ac7f8670b
Michiel-Janssen <157011760+MichielJanssenEHB@users.noreply.github.com>
2025-06-02 14:10:36 +0200

    remove static path

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ app.use((req, res, next) => {
 });
 
 // Frontend
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('/*\w', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/dist/index.html'));
-});
+// app.get('/*\w', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/client/dist/index.html'));
+// });
 
 // app.use(express.static(path.join(__dirname, '/www')));
 
